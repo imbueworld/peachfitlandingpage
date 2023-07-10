03f3a07b07eb3be65833c4ef8847a6c4c3c223a2
carrabre <40183833+carrabre@users.noreply.github.com>
2023-07-10 16:28:45 -0700

    updated text

--- components/features.tsx
+++ components/features.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { clear } from 'console';
 import { useState } from 'react';
 import Slider from 'react-input-slider';
 
@@ -8,9 +9,9 @@ export default function Features() {
 
   const [x, setX] = useState(100);
 
-const calculateEarnings = (membershipCost: number) => {
-  return membershipCost * 0.1475;
-};
+  const calculateEarnings = (membershipCost: number) => {
+    return membershipCost * 0.1475;
+  };
 
 
   return (
@@ -47,7 +48,7 @@ const calculateEarnings = (membershipCost: number) => {
             {/* 1st item */}
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <rect  width="64" height="64" rx="32" />
                 <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
               </svg>
@@ -58,7 +59,7 @@ const calculateEarnings = (membershipCost: number) => {
             {/* 2nd item */}
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <circle className="fill-current text-purple-600" cx="32" cy="32" r="32" />
+                <circle  cx="32" cy="32" r="32" />
                 <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
               </svg>
@@ -69,7 +70,7 @@ const calculateEarnings = (membershipCost: number) => {
             {/* 3rd item */}
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <rect width="64" height="64" rx="32" />
                 <g transform="translate(21 21)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
                   <ellipse className="stroke-current text-purple-300" cx="11" cy="11" rx="5.5" ry="11" />
                   <path className="stroke-current text-purple-100" d="M11 0v22M0 11h22" />
@@ -78,6 +79,46 @@ const calculateEarnings = (membershipCost: number) => {
               </svg>
               <h4 className="h4 mb-2">Higher Payouts</h4>
               <p className="text-lg text-gray-400 text-center">A new proprietary payout model ensures that gyms are paid out better than the competitors in the space, in many cases 3x more.</p>
+            </div>
+
+            {/* 4th item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect width="64" height="64" rx="32" />
+                <g transform="translate(22 21)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
+                  <path className="stroke-current text-purple-100" d="M17 22v-6.3a8.97 8.97 0 003-6.569A9.1 9.1 0 0011.262 0 9 9 0 002 9v1l-2 5 2 1v4a2 2 0 002 2h4a5 5 0 005-5v-5" />
+                  <circle className="stroke-current text-purple-300" cx="13" cy="9" r="3" />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Your Gym, Empowered</h4>
+              <p className="text-lg text-gray-400 text-center">Take the reins with Peach Fit. Reap the rewards of higher payouts than competing platforms, and voice your perspectives in shaping the direction of the company. Peach Fit is a platform that stands for you, the service provider.</p>
+            </div>
+
+            {/* 5th item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="400" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect width="64" height="64" rx="32" />
+                <g strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
+                  <path className="stroke-current text-purple-100" d="M29 42h10.229a2 2 0 001.912-1.412l2.769-9A2 2 0 0042 29h-7v-4c0-2.373-1.251-3.494-2.764-3.86a1.006 1.006 0 00-1.236.979V26l-5 6" />
+                  <path className="stroke-current text-purple-300" d="M22 30h4v12h-4z" />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">A Partner, Not a Provider</h4>
+              <p className="text-lg text-gray-400 text-center">We invite you to join us not only as a service provider but as a stakeholder. Peach Fit's innovative model offers you a stake in the business, so we flourish together. Let's transform the fitness industry into a more rewarding and sustainable venture.</p>
+            </div>
+
+            {/* 6th item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="500" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect width="64" height="64" rx="32" />
+                <g transform="translate(21 22)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
+                  <path className="stroke-current text-purple-300" d="M17 2V0M19.121 2.879l1.415-1.415M20 5h2M19.121 7.121l1.415 1.415M17 8v2M14.879 7.121l-1.415 1.415M14 5h-2M14.879 2.879l-1.415-1.415" />
+                  <circle className="stroke-current text-purple-300" cx="17" cy="5" r="3" />
+                  <path className="stroke-current text-purple-100" d="M8.86 1.18C3.8 1.988 0 5.6 0 10c0 5 4.9 9 11 9a10.55 10.55 0 003.1-.4L20 21l-.6-5.2a9.125 9.125 0 001.991-2.948" />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Quality and Diversity</h4>
+              <p className="text-lg text-gray-400 text-center">Embrace the opportunity to showcase your offerings on a dynamic platform that values quality and diversity. Connect with health and fitness enthusiasts across the nation who are eager to explore what you have to offer.</p>
             </div>
 
           </div>
--- components/hero.tsx
+++ components/hero.tsx
@@ -35,8 +35,10 @@ export default function Hero() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">Peach Fit</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">A gym owned fitness network where gyms earn up to 3x per booking than competitors, all while become an owner in the network building a stronger fitness community for all.</p>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">The first 1,000 locations to sign up receive in aggregate <b>10% ownership</b> in the network.</p>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">
+            It's not just a fitness platform, it's a movement towards stronger, more unified fitness communities. Gyms don't just earn up to 3x more than competitors, they become owners in the network.         
+            </p>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">The first 1,000 locations to sign up receive in aggregate <b>10% ownership</b> in the network. Be the first to know when we launch.</p>
             {/* Iframe */}
           <div id="getWaitlistContainer" data-waitlist_id="8890" data-widget_type="WIDGET_2" className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"></div>
           <link rel="stylesheet" type="text/css" href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css" />
